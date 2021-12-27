6a547cbe373b15bf70b5f9f747d0e5d9ef7c3535
zosman6 <zosman6@gatech.edu>
2021-12-26 23:57:52 -0600

    Initial skeleton for Phonebook WebApp

---
+++ myContactsApp/src/test/java/com/contacts/myContactsApp/MyContactsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contacts.myContactsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyContactsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
