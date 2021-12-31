7bb9001b3ef4860fb160fe6d0a38a92bae34dbe0
zosman9 <zosman007@gmail.com>
2021-12-30 23:04:11 -0600

    Skeleton using Intellij U

---
+++ src/main/java/com/example/simplecrudphonebookapp/SimpleCrudPhonebookAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplecrudphonebookapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudPhonebookAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCrudPhonebookAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simplecrudphonebookapp/SimpleCrudPhonebookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplecrudphonebookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudPhonebookAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
